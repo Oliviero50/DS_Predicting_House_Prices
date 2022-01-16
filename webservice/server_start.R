12f78052146a5c9f40d467110d44f7ba4716a200
[your <oliver.tomondy@sigmatek.at>
2022-01-16 17:33:29 +0100

    Fixed not relative path

--- webservice/server_start.R
+++ webservice/server_start.R
@@ -1,7 +1,7 @@
 library(plumber)
 
 # Falls die Datei nicht gefunden wird, geben Sie hier den absoluten Pfad zum server.R an.
-path = "C:/Users/tomoli/Desktop/Skola/Data Science/DS_Predicting_House_Prices/webservice/server.R"
+path = "server.R"
 
 ws = plumb(path)
 ws$run(host="127.0.0.1", port = 8080)
